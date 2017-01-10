d625bd40ca749d49b31c61eb5fafb05f38e70728
manueldl <mdomlop@gmail.com>
2017-01-10 12:46:13 +0100

    Updated from makefile

--- makefile
+++ makefile
@@ -1,24 +1,29 @@
 THEMENAME='retrosmart'
-SUFFIX='-openbox-themes'
+SUFFIX='-openbox-themes-out'
 NAME=$(THEMENAME)$(SUFFIX)
 SRCDIR='src'
 OUTDIR='.'
 PREFIX='/usr'
 INSTALLDIR=$(PREFIX)'/share/themes'
+export ROOT_DIR=${PWD}
 
-$(NAME): clean
+$(NAME): clean build
+
+build:
 	mkdir -p $(OUTDIR)/$(NAME)
-	for i in $(SRCDIR)/$(THEMENAME)-*; \
-	    do \
-	    name=`basename "$$i"`; \
-	    dir=$(OUTDIR)/$(NAME)/$$name/openbox-3; \
-	    mkdir -p "$$dir"; \
-	    cp "$$i"/themerc "$$dir"/; \
-	    cp $(SRCDIR)/pixmaps/* "$$dir"; \
+	cd $(OUTDIR)/$(NAME) && \
+	python $(ROOT_DIR)/src/mkobtheme.py $(ROOT_DIR)/src/rc $(ROOT_DIR)/src/mkrc.py
+	for i in $(OUTDIR)/$(NAME)/*; \
+		do \
+		name=`basename "$$i"`; \
+		dir=$(OUTDIR)/$(NAME)/$$name/openbox-3; \
+		mkdir -p "$$dir"; \
+		mv "$$i"/themerc "$$dir"/; \
+		cp $(SRCDIR)/pixmaps/* "$$dir"; \
 	done
 
-
 install: uninstall $(NAME)
+	install -d -m 755 $(INSTALLDIR)
 	cp -r $(OUTDIR)/$(NAME)/* $(INSTALLDIR)
 	chown -R root:root $(INSTALLDIR)/$(THEMENAME)-openbox-*/
 	chmod -R u=rwX,go=rX $(INSTALLDIR)/$(THEMENAME)-openbox-*/
@@ -29,3 +34,7 @@ uninstall:
 clean:
 	rm -Rf $(OUTDIR)/$(NAME)
 
+togit:
+	git add .
+	git commit -m "Updated from makefile"
+	git push origin
--- src/mkobtheme.py
+++ src/mkobtheme.py
@@ -1,218 +1,51 @@
 #!/usr/bin/env python
+# Generate differents themerc from a single rc file by calling mkrc.py
 
-# mkobtheme --light white --dark black --active blue --inactive darkgrey
-# --box grey --notify yellow --menu orange -w red
+import subprocess
+import sys
 
-import argparse
+themes = {}
 
-rc = {}
+if sys.argv[1]:
+    fname = sys.argv[1]
+else:
+    fname = 'src/rc'
 
+if sys.argv[2]:
+    mkrc = ('python', sys.argv[2])
+else:
+    mkrc = ('python', 'src/mkrc.py')
 
-def parseargs():
-    global args
-    # Default options:
-    light = '#ffffff'
-    dark = '#000000'
-    active = '#0000ff'
-    inactive = '#666666'
-    shadow = '#999999'
-    box = '#cccccc'
-    notify = '#ffcc00'
-    warning = '#990000'
-    clean = False
+with open(fname) as f:
+    for line in f:
+        if not line.strip():
+            continue
+        else:
+            k, v = line.split('=')
+            themes[k.strip()] = v.strip()
 
-    parser = argparse.ArgumentParser(
-            prog='mkobtheme',
-            description='Make an openbox theme'
-            )
-    parser.add_argument('-l', '--light', default=light)
-    parser.add_argument('-d', '--dark', default=dark)
-    parser.add_argument('-a', '--active', default=active)
-    parser.add_argument('-i', '--inactive', default=inactive)
-    parser.add_argument('-s', '--shadow', default=shadow)
-    parser.add_argument('-b', '--box', default=box)
-    parser.add_argument('-n', '--notify', default=notify)
-    parser.add_argument('-w', '--warning', default=warning)
-    parser.add_argument('-m', '--menu')
-    parser.add_argument('-C', '--clean', default=clean, action='store_true')
+for t in themes:
+    dname = 'retrosmart-openbox-' + t
+    dnameb = 'retrosmart-openbox-' + t + '-nobuttons'
+    md = ('mkdir', dname)
+    mdb = ('mkdir', dnameb)
+    subprocess.call(md)
+    subprocess.call(mdb)
+    f = dname + '/themerc'
+    fb = dnameb + '/themerc'
+    if ':' in themes[t]:
+        active, menu = themes[t].split(':')
+        active = '#' + active
+        menu = '#' + menu
 
-    args = parser.parse_args()
-
-
-def automatic(hexstring):
-    if hexstring.startswith('#'):
-        hexstring = hexstring[1:]
-
-    r, g, b = tuple(bytes.fromhex(hexstring))
-
-    if (r * 0.299 + g * 0.587 + b * 0.114) > 186:
-        color = 'black'
+        mkt = (mkrc[0], mkrc[1], '-a', active, '-m', menu)
+        mktb = (mkrc[0], mkrc[1], '-C', '-a', active, '-m', menu)
     else:
-        color = 'white'
+        active = '#' + themes[t]
+        mkt = (mkrc[0], mkrc[1], '-a', active)
+        mktb = (mkrc[0], mkrc[1], '-C', '-a', active)
+    with open(f, "w") as outfile:
+        subprocess.call(mkt, stdout=outfile)
+    with open(fb, "w") as outfile:
+        subprocess.call(mktb, stdout=outfile)
 
-    return(color)
-
-
-def print_rc():
-    for k in rc:
-        print(k + ': ' + rc[k])
-
-
-def main():
-    parseargs()
-
-    number = '0'
-    rc['window.client.padding.height'] = number
-
-    number = '2'
-    rc['border.width'] = number
-    rc['menu.separator.width'] = number
-
-    number = '5'
-    rc['menu.overlap'] = number
-    rc['padding.height'] = number
-
-    number = '7'
-    rc['padding.width'] = number
-    rc['window.client.padding.width'] = number
-    rc['window.handle.width'] = number
-
-    justify = 'left'
-    rc['menu.items.justify'] = justify
-    rc['menu.title.text.justify'] = justify
-    rc['window.label.text.justify'] = justify
-
-    texture = 'flat solid'
-    rc['window.inactive.label.bg'] = texture
-    rc['menu.items.active.bg'] = texture
-    rc['menu.items.bg'] = texture
-    rc['menu.title.bg'] = texture
-    rc['osd.bg'] = texture
-    rc['osd.hilight.bg'] = texture
-    rc['osd.label.bg'] = texture
-    rc['osd.unhilight.bg'] = texture
-    rc['window.active.grip.bg'] = texture
-    rc['window.active.handle.bg'] = texture
-    rc['window.active.label.bg'] = texture
-    rc['window.active.title.bg'] = texture
-    rc['window.inactive.grip.bg'] = texture
-    rc['window.inactive.handle.bg'] = texture
-    rc['window.inactive.title.bg'] = texture
-
-    texture = 'flat solid border'
-    rc['osd.button.focused.bg'] = texture
-    rc['osd.button.pressed.bg'] = texture
-    rc['osd.button.unpressed.bg'] = texture
-    rc['window.active.button.toggled.bg'] = texture
-    rc['window.inactive.button.toggled.bg'] = texture
-    rc['window.active.button.disabled.bg'] = texture
-    rc['window.inactive.button.disabled.bg'] = texture
-    rc['window.active.button.pressed.bg'] = texture
-    if args.clean:
-        texture = 'flat solid'
-    rc['window.inactive.button.pressed.bg'] = texture
-    rc['window.active.button.unpressed.bg'] = texture
-    rc['window.inactive.button.unpressed.bg'] = texture
-    rc['window.active.button.hover.bg'] = texture
-    rc['window.inactive.button.hover.bg'] = texture
-
-    color = args.light
-    rc['osd.button.pressed.box.color'] = color
-    rc['window.active.button.disabled.bg.border.color'] = color
-    rc['window.active.button.disabled.image.color'] = color
-    rc['window.inactive.button.disabled.bg.border.color'] = color
-    rc['window.inactive.button.disabled.image.color'] = color
-
-    color = args.dark
-    rc['border.color'] = color
-    rc['osd.button.focused.box.color'] = color
-    rc['window.active.button.toggled.bg.border.color'] = color
-    rc['window.active.button.toggled.image.color'] = color
-    rc['window.inactive.button.toggled.bg.border.color'] = color
-    rc['window.inactive.button.toggled.image.color'] = color
-    if args.clean:
-        color = args.active
-    rc['window.active.button.hover.image.color'] = color
-    rc['window.active.button.pressed.image.color'] = color
-    rc['window.active.button.unpressed.image.color'] = color
-    if args.clean:
-        color = args.inactive
-    rc['window.inactive.button.hover.image.color'] = color
-    rc['window.inactive.button.unpressed.image.color'] = color
-
-    color = args.active
-    rc['osd.hilight.bg.color'] = color
-    rc['window.active.client.color'] = color
-    rc['window.active.grip.bg.color'] = color
-    rc['window.active.handle.bg.color'] = color
-    rc['window.active.label.bg.color'] = color
-    rc['window.active.title.bg.color'] = color
-    rc['window.active.title.separator.color'] = color
-    rc['window.inactive.button.pressed.bg.color'] = color
-
-    rc['window.active.label.text.color'] = automatic(color)
-
-    color = args.inactive
-    rc['menu.title.bg.color'] = color
-    rc['window.inactive.client.color'] = color
-    rc['window.inactive.grip.bg.color'] = color
-    rc['window.inactive.handle.bg.color'] = color
-    rc['window.inactive.label.bg.color'] = color
-    rc['window.inactive.title.bg.color'] = color
-    rc['window.inactive.title.separator.color'] = color
-    rc['menu.items.disabled.text.color'] = color
-
-    rc['menu.title.text.color'] = automatic(color)
-
-    color = args.shadow
-    rc['osd.button.focused.bg.color'] = color
-
-    rc['osd.button.focused.text.color'] = automatic(color)
-
-    if args.clean:
-        color = args.active
-    rc['window.active.button.hover.bg.color'] = color
-    rc['window.active.button.pressed.bg.color'] = color
-    if args.clean:
-        color = args.inactive
-    rc['window.inactive.button.hover.bg.color'] = color
-
-    color = args.box
-    rc['osd.bg.color'] = color
-    rc['osd.button.unpressed.bg.color'] = color
-    rc['osd.label.bg.color'] = color
-    rc['menu.items.bg.color'] = color
-    rc['osd.unhilight.bg.color'] = color
-    rc['window.inactive.label.text.color'] = color
-
-    rc['menu.items.text.color'] = automatic(color)
-    rc['osd.button.unpressed.text.color'] = automatic(color)
-    rc['osd.label.text.color'] = automatic(color)
-
-    if args.clean:
-        color = args.active
-    rc['window.active.button.unpressed.bg.color'] = color
-    if args.clean:
-        color = args.inactive
-    rc['window.inactive.button.unpressed.bg.color'] = color
-
-    color = args.warning
-    rc['window.active.button.disabled.bg.color'] = color
-    rc['window.inactive.button.disabled.bg.color'] = color
-
-    color = args.notify
-    rc['window.active.button.toggled.bg.color'] = color
-    rc['window.inactive.button.toggled.bg.color'] = color
-
-    color = args.menu or args.active
-    rc['menu.items.active.bg.color'] = color
-    rc['osd.button.pressed.bg.color'] = color
-
-    rc['menu.items.active.text.color'] = automatic(color)
-    rc['osd.button.pressed.text.color'] = automatic(color)
-
-    print_rc()
-
-
-if __name__ == '__main__':
-    main()
