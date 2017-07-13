111c046c66ba62507024e1f66aebd936e15fc9c1
manueldl <mdomlop@gmail.com>
2017-07-13 08:49:27 +0200

    Cleaned build system

--- makefile.mk
+++ makefile.mk
@@ -1,50 +1,36 @@
-NAME := 'retrosmart-openbox'
-PREFIX='/usr'
-DESTDIR=''
+BASE := 'retrosmart-openbox'
+NAME := $(BASE)-themes
+PREFIX := '/usr/local'
 TEMPDIR := $(shell mktemp -u --suffix .$(NAME))
 
 $(NAME):
 
 install:
-	install -d -m 755 $(DESTDIR)/$(PREFIX)'/share/themes'
-	cp -r $(NAME)-* $(DESTDIR)/$(PREFIX)'/share/themes'
-	chown -R root:root $(DESTDIR)/$(PREFIX)'/share/themes'/$(NAME)-*/
-	chmod -R u=rwX,go=rX $(DESTDIR)/$(PREFIX)'/share/themes'/$(NAME)-*/
-	install -Dm 644 AUTHORS $(DESTDIR)/$(PREFIX)/share/doc/$(NAME)/AUTHORS
-	install -Dm 644 COPYING $(DESTDIR)/$(PREFIX)/share/licenses/$(NAME)/COPYING
-	install -Dm 644 INSTALL $(DESTDIR)/$(PREFIX)/share/doc/$(NAME)/INSTALL
-	install -Dm 644 README $(DESTDIR)/$(PREFIX)/share/doc/$(NAME)/README
+	install -d -m 755 $(PREFIX)'/share/themes'
+	cp -r $(BASE)-* $(PREFIX)'/share/themes'
+	chown -R root:root $(PREFIX)'/share/themes'/$(BASE)-*/
+	chmod -R u=rwX,go=rX $(PREFIX)'/share/themes'/$(BASE)-*/
+	install -Dm 644 AUTHORS $(PREFIX)/share/doc/$(NAME)/AUTHORS
+	install -Dm 644 COPYING $(PREFIX)/share/licenses/$(NAME)/COPYING
+	install -Dm 644 INSTALL $(PREFIX)/share/doc/$(NAME)/INSTALL
+	install -Dm 644 README $(PREFIX)/share/doc/$(NAME)/README
 
 uninstall:
-	rm -rf $(PREFIX)/share/themes/$(NAME)-*/
+	rm -rf $(PREFIX)/share/themes/$(BASE)-*/
 	rm -rf $(PREFIX)/share/licenses/$(NAME)/
 	rm -rf $(PREFIX)/share/doc/$(NAME)/
 
 clean:
-	rm -rf $(NAME)-* /tmp/tmp.*.$(NAME)
+	rm -rf $(BASE)-* /tmp/tmp.*.$(BASE) README.md AUTHORS.md INSTALL.md
 
 purge: clean
-	rm -f makefile README.md AUTHORS.md INSTALL.md
+	rm -f makefile
 	@echo makefile deleted. Execute configure script to generate it again.
 
-togit: purge doc
-	git add .
-	git commit -m 'Updated from makefile'
-	git push origin
-
-pacman-remote: clean
+pkg:
 	mkdir $(TEMPDIR)
-	cp packages/pacman/PKGBUILD $(TEMPDIR)/
-	cd $(TEMPDIR); makepkg
-	cp $(TEMPDIR)/$(NAME)-*.pkg.tar.xz .
-	@echo Package done!
-	@echo You can install it as root with:
-	@echo pacman -U $(NAME)-*.pkg.tar.xz
-
-pacman: clean
-	mkdir $(TEMPDIR)
-	tar cf $(TEMPDIR)/$(NAME)-themes.tar ../$(NAME)-themes
-	cp packages/pacman/local/PKGBUILD $(TEMPDIR)/
+	tar cf $(TEMPDIR)/$(NAME).tar ../$(NAME)
+	cp packages/PKGBUILD $(TEMPDIR)/
 	cd $(TEMPDIR); makepkg
 	cp $(TEMPDIR)/$(NAME)-*.pkg.tar.xz .
 	@echo Package done!
